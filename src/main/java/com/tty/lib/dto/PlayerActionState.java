4c9da718766e2c6c928aa14222b8612e345f5f5b
SuperArilo <superarilowh@gmail.com>
2025-12-13 17:46:30 +0800

    修复 action 在关闭服务器时候无法正确移除效果云

--- src/main/java/com/tty/lib/dto/PlayerActionState.java
+++ src/main/java/com/tty/lib/dto/PlayerActionState.java
@@ -1,5 +1,6 @@
 package com.tty.lib.dto;
 
+import com.tty.lib.Log;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Entity;
@@ -12,6 +13,15 @@ public class PlayerActionState extends State {
 
     public PlayerActionState(Entity owner) {
         super(owner, Integer.MAX_VALUE);
+    }
+
+    public void removeToolEntity() {
+        if (this.tool_entity == null) return;
+        this.getOwner().eject();
+        this.tool_entity.remove();
+        this.tool_entity = null;
+        this.setOver(true);
+        Log.debug("player %s ejected, remove tool entity", this.getOwner().getName());
     }
 
 }
