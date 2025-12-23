67f6632b4b79c3cfac7dc70347c1c8371e2af27c
SuperArilo <728152568@qq.com>
2025-12-23 09:09:49 +0800

    修复 folia 下移除 tool_entity 出现的线程错误问题

--- src/main/java/com/tty/lib/dto/PlayerActionState.java
+++ src/main/java/com/tty/lib/dto/PlayerActionState.java
@@ -1,9 +1,11 @@
 package com.tty.lib.dto;
 
+import com.tty.lib.Lib;
 import com.tty.lib.Log;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Entity;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerActionState extends State {
 
@@ -15,13 +17,18 @@ public class PlayerActionState extends State {
         super(owner, Integer.MAX_VALUE);
     }
 
-    public void removeToolEntity() {
+    public void removeToolEntity(JavaPlugin plugin) {
         if (this.tool_entity == null) return;
-        this.getOwner().eject();
-        this.tool_entity.remove();
-        this.tool_entity = null;
-        this.setOver(true);
-        Log.debug("player %s ejected, remove tool entity", this.getOwner().getName());
+        Lib.Scheduler.runAtEntity(plugin,
+            this.tool_entity,
+            i-> {
+                this.getOwner().eject();
+                this.tool_entity.remove();
+                this.tool_entity = null;
+                this.setOver(true);
+                Log.debug("player %s ejected, remove tool entity", this.getOwner().getName());
+            },
+            () -> Log.error("remove tool_entity error."));
     }
 
 }
