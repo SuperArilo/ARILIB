c69c39b26ff24464b5336bb57673fe9ec721f499
SuperArilo <728152568@qq.com>
2026-01-19 12:36:47 +0800

    更换占位符

--- src/main/java/com/tty/lib/enum_type/LangType.java
+++ src/main/java/com/tty/lib/enum_type/LangType.java
@@ -2,21 +2,12 @@ package com.tty.lib.enum_type;
 
 public enum LangType implements LangTypeEnum {
     PERIOD_UNRESOLVED("period"),
-    SOURCE_DISPLAY_NAME_UNRESOLVED("source_display_name"),
-    CHAT_MESSAGE_UNRESOLVED("message"),
+
     PLAYER_NAME_UNRESOLVED("player_name"),
 
-    KILLER("killer"),
-    VICTIM("victim"),
-    KILLER_ITEM("killer_item"),
     BAN_T0TAL_TIME("ban_total_time"),
     BAN_REASON("ban_reason"),
-    BAN_END_TIME("ban_end_time"),
-    MOB("mob"),
-    MOB_CURRENT_HEALTH("mob_current_health"),
-    MOB_MAX_HEALTH("mob_max_health"),
-    ENCHANT_NAME("enchant_name"),
-    ENCHANT_LEVEL("enchant_level");
+    BAN_END_TIME("ban_end_time");
 
     private final String type;
 
