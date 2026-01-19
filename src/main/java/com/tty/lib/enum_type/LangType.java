03e13c392574635de84e22581c6660a8ec09a3e1
SuperArilo <728152568@qq.com>
2026-01-19 13:09:38 +0800

    将 ban 逻辑迁移到占位符引擎

--- src/main/java/com/tty/lib/enum_type/LangType.java
+++ src/main/java/com/tty/lib/enum_type/LangType.java
@@ -3,11 +3,7 @@ package com.tty.lib.enum_type;
 public enum LangType implements LangTypeEnum {
     PERIOD_UNRESOLVED("period"),
 
-    PLAYER_NAME_UNRESOLVED("player_name"),
-
-    BAN_T0TAL_TIME("ban_total_time"),
-    BAN_REASON("ban_reason"),
-    BAN_END_TIME("ban_end_time");
+    PLAYER_NAME_UNRESOLVED("player_name");
 
     private final String type;
 
