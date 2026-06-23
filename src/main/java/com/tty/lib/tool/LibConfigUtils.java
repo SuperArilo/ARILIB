6cf3725f195fd2cded860b91c2d31ccc705c69c6
SuperArilo <728152568@qq.com>
2026-06-23 16:56:42 +0800

    更新 api 方法

--- src/main/java/com/tty/lib/tool/LibConfigUtils.java
+++ src/main/java/com/tty/lib/tool/LibConfigUtils.java
@@ -1,6 +1,5 @@
 package com.tty.lib.tool;
 
-import com.tty.api.utils.ComponentUtils;
 import com.tty.lib.Lib;
 import com.tty.lib.enum_type.FilePath;
 import net.kyori.adventure.text.TextComponent;
@@ -13,8 +12,7 @@ public class LibConfigUtils {
      * @return 返回构建完成的 Component
      */
     public static TextComponent t(String key) {
-        return ComponentUtils.text(Lib.instance.getConfigInstance().getValue(key, FilePath.Lang));
+        return Lib.instance.getComponentTool().text(Lib.instance.getConfigInstance().getValue(key, FilePath.Lang));
     }
-
 
 }
