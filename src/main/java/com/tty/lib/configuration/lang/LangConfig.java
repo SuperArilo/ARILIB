5fdcc517210b5d02ad9b03305e867e513a5dcaa4
SuperArilo <superarilowh@gmail.com>
2026-07-04 16:46:13 +0800

    添加默认构造函数保证启动

--- src/main/java/com/tty/lib/configuration/lang/LangConfig.java
+++ src/main/java/com/tty/lib/configuration/lang/LangConfig.java
@@ -1,11 +1,17 @@
 package com.tty.lib.configuration.lang;
 
+import com.tty.api.AbstractJavaPlugin;
 import com.tty.api.configuration.LangConfiguration;
 import com.tty.lib.Lib;
 import com.tty.lib.enum_type.LangFile;
 
 public class LangConfig extends LangConfiguration {
+
     public LangConfig() {
         super(Lib.instance, LangFile.LANG.getPath());
     }
+    public LangConfig(AbstractJavaPlugin plugin) {
+        super(plugin);
+    }
+
 }
