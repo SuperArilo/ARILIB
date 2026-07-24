eaa1d1703faccc9d42a9b3d79cc4720a20604323
SuperArilo <superarilowh@gmail.com>
2026-07-24 20:04:32 +0800

    更新传送方法

--- src/main/java/com/tty/lib/services/impl/TeleportingServiceImpl.java
+++ src/main/java/com/tty/lib/services/impl/TeleportingServiceImpl.java
@@ -41,7 +41,7 @@ public class TeleportingServiceImpl implements TeleportingService {
     }
 
     @Override
-    public TeleportingService teleport(Entity entity, Location beforeLocation, Location targetLocation) {
+    public TeleportingService teleport(Entity entity, Location targetLocation) {
         if (!this.status || !(entity instanceof Player player)) {
             this.aborted.run();
             return this;
@@ -51,6 +51,8 @@ public class TeleportingServiceImpl implements TeleportingService {
             this.before.accept(this);
         }
 
+
+
         Lib.instance.getScheduler().runAtRegion(targetLocation, i -> {
             for (int y = 0;y <= targetLocation.getWorld().getMaxHeight();y++) {
                 if (targetLocation.clone().add(0, y, 0).getBlock().isEmpty()) {
@@ -59,26 +61,32 @@ public class TeleportingServiceImpl implements TeleportingService {
                 }
             }
 
-            boolean isFolia = Scheduler.isFolia();
-            boolean cancelled = false;
+            Lib.instance.getScheduler().runAtEntity(entity, t -> {
+                boolean isFolia = Scheduler.isFolia();
+                boolean cancelled = false;
 
-            if (isFolia) {
-                PlayerTeleportEvent event = new PlayerTeleportEvent(player, beforeLocation, targetLocation, PlayerTeleportEvent.TeleportCause.PLUGIN);
-                cancelled = !event.callEvent();
-            }
-
-            if (cancelled) {
-                this.after.run();
-                entity.sendMessage(LibConfigUtils.t("function.teleport.error"));
-                return;
-            }
-
-            entity.teleportAsync(targetLocation, PlayerTeleportEvent.TeleportCause.PLUGIN).thenAccept(status -> {
-                if (status) {
-                    Lib.instance.getScheduler().runAtEntity(player, t -> player.playSound(Sound.sound(ENTITY_ENDER_EYE_DEATH, SoundCategory.PLAYERS, 1.0f, 1.0f)), null);
+                if (isFolia) {
+                    PlayerTeleportEvent event = new PlayerTeleportEvent(player, entity.getLocation(), targetLocation, PlayerTeleportEvent.TeleportCause.PLUGIN);
+                    cancelled = !event.callEvent();
                 }
-                this.after.run();
-                entity.sendMessage(LibConfigUtils.t(status ? "function.teleport.success":"function.teleport.error"));
+
+                if (cancelled) {
+                    this.after.run();
+                    entity.sendMessage(LibConfigUtils.t("function.teleport.error"));
+                    return;
+                }
+
+                entity.teleportAsync(targetLocation, PlayerTeleportEvent.TeleportCause.PLUGIN).thenAccept(status -> {
+                    if (status) {
+                        Lib.instance.getScheduler().runAtEntity(player, o -> player.playSound(Sound.sound(ENTITY_ENDER_EYE_DEATH, SoundCategory.PLAYERS, 1.0f, 1.0f)), null);
+                    }
+                    this.after.run();
+                    entity.sendMessage(LibConfigUtils.t(status ? "function.teleport.success":"function.teleport.error"));
+                });
+            },
+            () -> {
+                this.status = false;
+                entity.sendMessage(LibConfigUtils.t("function.teleport.error"));
             });
         });
         return this;
